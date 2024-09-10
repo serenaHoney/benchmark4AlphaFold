98c0908795f7e4216d465b64020164474a3aeb03
Elizabeth Holland <119709832+serenaHoney@users.noreply.github.com>
2024-09-10 19:07:42 +0100

    Create alphafold_api.py

---
+++ alphafold_api.py
@@ -0,0 +1,4 @@
+"""The AlphaFold Protein Structure Database API grants access to information about protein structures predicted by AlphaFold, 
+such as URLs to model files (mmCIF, bCIF and PDB), models quality estimates, etc.
+More info at https://alphafold.ebi.ac.uk/api-docs 
+"""
