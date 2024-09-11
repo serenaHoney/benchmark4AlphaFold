5e600af11c74614f28bcd6dcbc6e8e69e5bb7213
Elizabeth Holland <119709832+serenaHoney@users.noreply.github.com>
2024-09-11 18:54:26 +0100

    Update llm_api.py

--- llm_api.py
+++ llm_api.py
@@ -3,8 +3,12 @@ The OpenAI API uses API keys for authentication
 """
 
 from openai import OpenAI
+import os
+
+org_id = os.getenv('CHATGPT_ORG_ID')
+api_key = os.getenv('CHATGPT_API_KEY')
 
 client = OpenAI(
-  organization='YOUR_ORG_ID',
-  project='$PROJECT_ID',
+  organization = org_id,
+  project = api_key,
 )
